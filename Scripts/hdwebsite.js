50f6c46dfcccd3345a55f8d23c080703a83d60dc
Jian Huang <jianhuang16888@gmail.com>
2016-05-26 15:52:39 -0400

    Initial import

---
+++ Scripts/hdwebsite.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    $(window).on("load resize", function () {
+        $(".fill-screen-half").css("height", 500); 
+        $(".fill-screen-one-third").css("height", 300);
+    });
+
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: 200
+    });
+
+    $('nav a, .down-button a').bind('click', function () {
+        $('html, body').stop().animate({
+                scrollTop: $($(this).attr('href')).offset().top
+            },
+            1500,
+            'easeInOutExpo'
+        );
+        event.preventDefault();
+    });
+});
